012257b8ab0464770d79a4d48338448d6caa15a0
citizenmatt <m.t.ellis@gmail.com>
2011-08-23 07:17:01 +0100

    Initial commit. Created VS package + test projects and included initially required nuget packages

---
+++ ReSharperExtensionManager.Vs2010/Guids.cs
@@ -0,0 +1,14 @@
+﻿// Guids.cs
+// MUST match guids.h
+using System;
+
+namespace citizenmatt.ReSharperExtensionManager_Vs2010
+{
+    static class GuidList
+    {
+        public const string guidReSharperExtensionManager_Vs2010PkgString = "a32d3c3a-2b0e-4c64-bf33-4bb842e0f8f8";
+        public const string guidReSharperExtensionManager_Vs2010CmdSetString = "0236d38d-aba3-4c60-8e26-ce5040728439";
+
+        public static readonly Guid guidReSharperExtensionManager_Vs2010CmdSet = new Guid(guidReSharperExtensionManager_Vs2010CmdSetString);
+    };
+}
