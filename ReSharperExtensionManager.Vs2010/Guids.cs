22bac9c468f72f0fa5eb3f403818f3b502f19886
citizenmatt <m.t.ellis@gmail.com>
2011-08-23 07:25:34 +0100

    Renamed guids and package class

--- ReSharperExtensionManager.Vs2010/Guids.cs
+++ ReSharperExtensionManager.Vs2010/Guids.cs
@@ -1,15 +1,12 @@
-﻿// Guids.cs
-// MUST match guids.h
-
-using System;
+﻿using System;
 
 namespace CitizenMatt.ReSharper.ExtensionManager
 {
     static class GuidList
     {
-        public const string guidReSharperExtensionManager_Vs2010PkgString = "a32d3c3a-2b0e-4c64-bf33-4bb842e0f8f8";
-        public const string guidReSharperExtensionManager_Vs2010CmdSetString = "0236d38d-aba3-4c60-8e26-ce5040728439";
+        public const string VsPackageString = "a32d3c3a-2b0e-4c64-bf33-4bb842e0f8f8";
+        public const string VsCommandSetString = "0236d38d-aba3-4c60-8e26-ce5040728439";
 
-        public static readonly Guid guidReSharperExtensionManager_Vs2010CmdSet = new Guid(guidReSharperExtensionManager_Vs2010CmdSetString);
+        public static readonly Guid VsCommandSet = new Guid(VsCommandSetString);
     };
 }
---
+++ ReSharperExtensionManager.Vs2010/VsPackage.cs
@@ -0,0 +1,24 @@
+﻿using System.Diagnostics;
+using System.Globalization;
+using System.Runtime.InteropServices;
+using Microsoft.VisualStudio.Shell;
+
+namespace CitizenMatt.ReSharper.ExtensionManager
+{
+    [PackageRegistration(UseManagedResourcesOnly = true)]
+    [InstalledProductRegistration("#110", "#112", "1.0", IconResourceID = 400)]
+    [Guid(GuidList.VsPackageString)]
+    public sealed class VsPackage : Package
+    {
+        public VsPackage()
+        {
+            Trace.WriteLine(string.Format(CultureInfo.CurrentCulture, "Entering constructor for: {0}", this));
+        }
+
+        protected override void Initialize()
+        {
+            Trace.WriteLine (string.Format(CultureInfo.CurrentCulture, "Entering Initialize() of: {0}", this));
+            base.Initialize();
+        }
+    }
+}
