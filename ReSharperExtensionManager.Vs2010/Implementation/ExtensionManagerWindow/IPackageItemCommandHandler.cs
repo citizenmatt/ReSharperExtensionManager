541835024a32fdeae326cc6f5784d873ef03bce5
citizenmatt <m.t.ellis@gmail.com>
2011-10-18 22:33:25 +0100

    Added license

--- ReSharperExtensionManager.Vs2010/Implementation/ExtensionManagerWindow/IPackageItemCommandHandler.cs
+++ ReSharperExtensionManager.Vs2010/Implementation/ExtensionManagerWindow/IPackageItemCommandHandler.cs
@@ -1,3 +1,22 @@
+#region license
+// Copyright 2011 Matt Ellis (@citizenmatt)
+//
+// This file is part of ReSharper Extension Manager.
+//
+// ReSharper Extension Manager is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// ReSharper Extension Manager is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with ReSharper Extension Manager.  If not, see <http://www.gnu.org/licenses/>.
+#endregion
+
 namespace CitizenMatt.ReSharper.ExtensionManager.Implementation.ExtensionManagerWindow
 {
     public interface IPackageItemCommandHandler
